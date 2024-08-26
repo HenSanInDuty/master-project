6b5aa8097d2668c84580188940605683a61e46b6
Hen <heninduty@gmail.com>
2024-08-26 22:40:01 +0700

    NLP Inference Phase GUI

--- src/gui/pages/📑Multiple Document Summarizer.py
+++ src/gui/pages/📑Multiple Document Summarizer.py
@@ -38,8 +38,7 @@ async def main():
         with file_hint_col2:
             with st.popover("Bấm vào để xem dịnh dạng json", use_container_width=True):
                 st.markdown('''```json
-                            [{'topic':'Chủ đề của văn bản',
-'content':'Nội dung của văn bản'}]''')
+                            [{'topic':'Chủ đề của văn bản', 'content':'Nội dung của văn bản'}]''')
         document_uploaded_json = st.file_uploader("Tải tệp lên", accept_multiple_files= False, key="document_uploaded")
         
         # -- Chọn tập hư từ, có thể thêm file hoặc là thêm văn bảng hoặc không cần hư từ
@@ -82,8 +81,7 @@ async def main():
         with file_hint_col2:
             with st.popover("Bấm vào để xem dịnh dạng json", use_container_width=True):
                 st.markdown('''```json
-                            [{'topic':'Chủ đề của văn bản',
-'content':'Nội dung của văn bản'}]''')
+                            [{'topic':'Chủ đề của văn bản','content':'Nội dung của văn bản'}]''')
         document_uploaded_json = st.file_uploader("Tải tệp lên", accept_multiple_files= False, key="document_uploaded")
         
         # ----- Đưa vào mô hình chủ đề
@@ -108,14 +106,15 @@ async def main():
                 "summary_document":"Con mẹ nó. Nhức đầu vãi",
                 "sentences": ["Câu 1", "Câu 2", "Câu 3"],
                 "sentences_weight": [0.2, 0.2, 0.6],
-                "sentences_similarity": [["s1", "s2", 2], ["s1", "s3", 1]],
+                "sentences_similarity": [[0, 1, 2], [0, 2, 1]],
                 "metric_info": {
                     "ROGUE-2": 2.0,
                     "ROGUE-3": 3.0,
                     "ROGUE-n": 4.0 ,
                     "BERTScore": 5.0,
                     "Gemini-Review": "Tương đối ngon"
-                }
+                },
+                "number_sentence": 3
             }
             st.session_state.btn_result = True
             
@@ -136,12 +135,10 @@ async def main():
             # Tải tập tin tóm tắt
             st.download_button("Tải về nội dung tóm tắt", summarized_text)
                 
-            # -- Tab danh sách các câu
-            st.subheader("Danh sách câu")
-
-            # Sample tabs
+            # ----- Bảng thông tin tóm tắt của văn bản 
             tab1, tab2, tab3, tab4 = st.tabs(["Danh sách câu", "Thông tin câu", "Thông tin độ tương đồng", "Thông tin các độ đo"])
 
+            # -- Tab danh sách các câu
             with tab1:
                 sentences = result["sentences"]
 
@@ -152,20 +149,63 @@ async def main():
 
             # -- Tab thông tin của câu (trọng số)
             with tab2:
-                st.write("Thông tin câu...")
+                ratio_tab2 = [1, 3]
+                # Chia ra làm 2 cột, cột đầu là số thứ tự câu, cột 2 là trọng số của câu
+                header_tab2_col1, header_tab2_col2 = st.columns(ratio_tab2, vertical_alignment="center")
+                with header_tab2_col1:
+                    st.write("Câu")
+                with header_tab2_col2:
+                    st.write("Trọng số")
+                    
+                for i in range(result['number_sentence']):
+                    tab2_col1, tab2_col2 = st.columns(ratio_tab2, vertical_alignment="center")
+                    with tab2_col1:
+                        with st.popover(f"Câu {i+1}"):
+                            st.write(result['sentences'][i])
+                        
+                    with tab2_col2:
+                        font_large = f'<p style="font-size: 18px;">{result['sentences_weight'][i]}</p>'
+                        st.markdown(font_large, unsafe_allow_html=True)
 
             # -- Tab thông tin độ tương đồng
             with tab3:
-                st.write("Thông tin độ tương đồng...")
+                ratio_tab3 = [1, 1, 3]
+                # Chia làm 3 cột, cột 1 và 2 là số thứ tự câu, cột 3 là thông tin độ tương đồng
+                header_tab3_col1, header_tab3_col2, header_tab3_col3 = st.columns(ratio_tab3, vertical_alignment="center")
+                with header_tab3_col1:
+                    st.write("Câu")
+                with header_tab3_col2:
+                    st.write("Câu")
+                with header_tab3_col3:
+                    st.write("Độ tương đồng của câu")
+                    
+                for i in range(len(result['sentences_similarity'])):
+                    index_sentence_1, index_sentence_2, similarity = result['sentences_similarity'][i]
+                    tab3_col1, tab3_col2, tab3_col3 = st.columns(ratio_tab3, vertical_alignment="center")
+                    with tab3_col1:
+                        with st.popover(f"Câu {index_sentence_1 + 1}"):
+                            st.write(result['sentences'][index_sentence_1])
+                    
+                    with tab3_col2:
+                        with st.popover(f"Câu {index_sentence_2 + 1}"):
+                            st.write(result['sentences'][index_sentence_2])
+                        
+                    with tab3_col3:
+                        font_large = f'<p style="font-size: 18px;">{similarity}</p>'
+                        st.markdown(font_large, unsafe_allow_html=True)
             
             # -- Tab thông tin độ đo (ROGUE, ....)
             with tab4:
-                st.write("Thông tin các độ đo")
-
-
-    # TODO: CRAWL MODE
-    # elif mode == mode_choice[2]:
-    #     st.subheader("Hé lô, this is spider")
+                # Chia ra làm 3 tab
+                tab_rogue, tab_bertscore, tab_gemini = st.tabs(["ROGUE", "BertScore", "Gemini Review"])
+                with tab_rogue:
+                    st.write(f'ROGUE-2: {result["metric_info"]['ROGUE-2']}')
+                    st.write(f'ROGUE-3: {result["metric_info"]['ROGUE-3']}')
+                    st.write(f'ROGUE-n: {result["metric_info"]['ROGUE-n']}')
+                with tab_bertscore:
+                    st.write(f'BERTScore: {result["metric_info"]['BERTScore']}')
+                with tab_gemini:
+                    st.write(f'Gemini-Review: {result["metric_info"]["Gemini-Review"]}')
 
 if __name__ == "__main__":
     asyncio.run(main())
